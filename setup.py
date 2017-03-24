3eaa3fb6f0f99e9d592c24ee33fdd66f9cd66199
Thomas Stephens <thomas@ustudio.com>
2017-03-24 10:29:30 -0500

    Updating setup.py to list the client and server sub-packages, so they can be referenced from dependent projects

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ install_requires = [
 ]
 
 setup(name="ustudio-hmac-tornado",
-      version="0.1.0",
+      version="0.1.1",
       description="Simple HMAC Client/Server authentication for Tornado",
       url="https://github.com/ustudio/ustudio-hmac-tornado",
-      packages=["hmacauth"],
+      packages=["hmacauth", "hmacauth.client", "hmacauth.server"],
       install_requires=install_requires)
